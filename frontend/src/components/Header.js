095d872f873773599ab6013af2edd8a4a1f0db05
jyurick <jyurick@ualberta.ca>
2024-02-10 13:23:46 -0800

    added material ui and header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+
+const bcLogo = require('../assets/gov3_bc_logo.png').default;
 
 const Header = () => {
-  return (
-    <header>
-      <div className="logo">
-        CST Portal
-      </div>
-      <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Search</a></li>
-          <li><a href="#">Add Client</a></li>
-          <li><a href="#">Notes</a></li>
-          {/* Add more navigation links as needed */}
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Avatar
+                    src={bcLogo} // Path to your image file
+                    alt="BC Icon"
+                    sx={{ width: 80, height: 80, marginRight: 2 }} // Adjust size and spacing as needed
+                />
+                <nav>
+                    <Button color="inherit">Search</Button>
+                    <Button color="inherit">Add Client</Button>
+                    {/* Add more navigation links as needed */}
+                </nav>
+            </Toolbar>
+        </AppBar>
+    );
 }
 
 export default Header;
