c433f631f1d0ab96b636cd7872d6e8cf0abb502b
jyurick <jyurick@ualberta.ca>
2024-02-10 13:30:38 -0800

    removed avatar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,19 +2,11 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-
-const bcLogo = require('../assets/gov3_bc_logo.png').default;
 
 const Header = () => {
     return (
         <AppBar position="static">
             <Toolbar>
-                <Avatar
-                    src={bcLogo} // Path to your image file
-                    alt="BC Icon"
-                    sx={{ width: 80, height: 80, marginRight: 2 }} // Adjust size and spacing as needed
-                />
                 <nav>
                     <Button color="inherit">Search</Button>
                     <Button color="inherit">Add Client</Button>
